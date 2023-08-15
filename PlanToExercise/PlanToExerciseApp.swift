52b668d51fce9c4455a72d4ccaf7596796fac9f8
Kelvin Gao <u6511753@au.edu>
2023-08-15 14:48:02 +0700

    Initial Commit

---
+++ PlanToExercise/PlanToExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanToExerciseApp.swift
+//  PlanToExercise
+//
+//  Created by Kelvin Gao  on 15/8/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct PlanToExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
